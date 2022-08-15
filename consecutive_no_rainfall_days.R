da87120ee72f9ed28687e1692668e5da65eb4901
william.henshaw <william.henshaw@ssaihq.com>
2022-08-14 23:18:16 -0700

    Utilized Derek's functions to calculate CDD across a raster stack and output one raster of max CDD

--- consecutive_no_rainfall_days.R
+++ consecutive_no_rainfall_days.R
@@ -1,39 +1,169 @@
-### Find consecutive no rain periods for each station
+# Created by: Billy Henshaw
+# Edited 8/14/2022
+# CDD
+### Find annual maximum consecutive dry days at each pixel from 1990-2014
+# To be run AFTER convert_to_int.R
 
-dat<-read.csv("daily_merge7_long.csv")
+library(devtools)
+library(doParallel)
+library(ncdf4)
+library(raster)
+library(dplyr)
+library(ClusterR)
+library(RColorBrewer)
 
-# format date column
-dat$date<-as.Date(dat$date, format = "%m/%d/%Y")
-head(dat)
+packages_vector <- c('ncdf4', 'raster', 'dplyr')
 
-# make list of station names
-list<-unique(dat$station_name)
+# identify number of cores for parallel processing
+UseCores <- detectCores() - 1
 
-library(data.table)
+# range of years in dataset
+years <- seq(1990, 2014)
 
-# make empty data frame
-dat3<-data.frame()
+# register chunk of cores for parallel processing
+cl <- makeCluster(UseCores)
+registerDoParallel(cl)
 
-for (i in list){
-  # subset for station
-  dat2<-dat[which(dat$station_name == i),]
-  
-  # remove "NA" value rows
-  dat2<-na.omit(dat2)
-  
-  # convert values to numeric
-  dat2$precip_mm<-as.numeric(dat2$precip_mm)
-  
-  head(dat2)
-  
-  # count consecutive same value days
-  dat2$consec<-with(dat2, (precip_mm == 0)*
-                      ave(precip_mm, rleid(precip_mm == 0),
-                          FUN=seq_along))
-  
-  # combine all into one dataframe
-  dat3<-rbind(dat3, dat2)
+# unregister parallel processing in case of errors
+unregister_dopar <- function() {
+  env <- foreach:::.foreachGlobals
+  rm(list=ls(name=env), pos=env)
 }
 
-head(dat3, 50)
-summary(dat3$consec)
+# set working directory here
+wd <- "F:/DATA/HawaiiEP/All_Islands_Daily_RF_asc"
+setwd(wd)
+
+# create year folders in workspace if they don't exist
+for (year in years) {
+  dir.create(file.path(wd, year), showWarnings = FALSE)  
+}
+
+# create a list of lists, with each list being one year of daily files.
+#  25 lists of 365 or 366 files
+data <- lapply(years, function(x) {
+  list.files(file.path(wd, x, 'int'), 
+             full.names = TRUE, 
+             all.files = TRUE,
+             pattern = '.tif$',
+             no.. = TRUE)
+}
+)
+
+# grab basenames of all files
+basenames <- lapply(
+  lapply(
+    years, 
+    function(x) {
+      list.files(file.path(wd, x, 'int'), 
+                 all.files = TRUE,
+                 pattern = '.tif$',
+                 no.. = TRUE)
+    }
+  ), 
+  function(y) {
+    tools::file_path_sans_ext(y)
+  }
+)
+
+
+# use first twenty files of 1990 as a sample for testing. Can comment out in prod
+sample <- data[[1]][1:20]
+
+beginCluster(UseCores)
+unregister_dopar()
+
+# uncomment for testing
+# rainyday_dir <- file.path(wd, '1990', 'rainyday_rc')
+# rc_dir <- file.path(wd, '1990', 'sdii_rc')
+# int_dir <- file.path(wd, '1990', 'int')
+# sdii_dir <-  file.path(wd, '1990', 'sdii')
+# r5d_dir <-  file.path(wd, '1990', 'r5d')
+# cdd_rc_dir <- file.path(wd, '1990', 'cdd_rc')
+# output_dir <- file.path(wd, '1990', 'cdd')
+
+# reclassification matrix for CDD (<1 mm = 1, everything else = 0)
+# since we are using integer rasters (rainfall * 100), use 100 as threshold
+rcl <- rbind(c(0, 100, 1),
+             c(100, 9999999, 0)
+)
+
+# function for finding the max consec dry days
+cdd <- function(y, val = 1) {
+  maxval <- with(rle(y), max(lengths[values == val]))
+  return(maxval)
+}
+
+beginCluster(UseCores)
+unregister_dopar()
+
+# run a for loop of each year's data with parallel processing
+foreach (i = 1:length(years), .packages = packages_vector) %dopar% {
+  
+  # create directories inside each year's folder if they don't exist
+  dir.create(file.path(wd, years[i], 'cdd'), showWarnings = FALSE)
+  dir.create(file.path(wd, years[i], 'cdd_rc'), showWarnings = FALSE)
+  
+  cdd_rc_dir <- file.path(wd, years[i], 'cdd_rc')
+  int_dir <- file.path(wd, years[i], 'int')
+  output_dir <-  file.path(wd, years[i], 'cdd')
+  
+  # create empty stack
+  x <- stack()
+  
+  # iterate through each file in the year
+  # for (day in sample) {
+  for (day in data[[i]]) {
+    
+    # prepare new file name
+    rc_file_name <- sub(pattern = int_dir, 
+                        replacement = cdd_rc_dir,
+                        paste0(tools::file_path_sans_ext(day),
+                               "_cdd_rc.tif"))
+    
+    # if the file exists already, go to next day. Allows quick
+    # resume after error. Be sure to delete the error output file
+    # before resuming!
+    if (file.exists(rc_file_name)) next
+    
+    # perform reclassification using reclass matrix
+    cat("reclassing", tools::file_path_sans_ext(day), '\n')
+    img_rc <- reclassify(
+      raster(day),
+      rcl,
+      include.lowest = TRUE)
+    
+    # write reclassed raster to output dir as Boolean datatype
+    writeRaster(img_rc,
+                filename = rc_file_name,
+                overwrite=T,
+                format="GTiff",
+                datatype="LOG1S")
+    
+    # add reclassed raster to stack
+    x <- stack(x, img_rc)
+    
+  }
+  
+  output_file_name <- file.path(output_dir,
+                                paste0(years[i],
+                                       "_cdd.tif"))
+  
+  cat("calculating cdd for", years[i], '\n')
+  
+  # perform sum aggregation on raster stack
+  img_cdd <- calc(x,
+                  cdd)
+  
+  # write cdd output to output dir as integer type
+  writeRaster(img_cdd,
+              filename =
+                output_file_name,
+              overwrite=T,
+              format="GTiff",
+              datatype="INT2S")
+  
+}
+
+# free up the cores used for parallel processing
+stopCluster(cl)
